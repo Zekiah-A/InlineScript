09597249d7f29210c6b67fff6806d4cf2427044b
Zekiah-A <zekiahamoako@outlook.com>
2023-01-11 12:45:20 +0000

    Start fixing errors

--- TSHtml/AnnotationBase.cs
+++ TSHtml/AnnotationBase.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace TSHtml;
 
 public abstract class AnnotationBase : IAnnotation
@@ -5,8 +7,9 @@ public abstract class AnnotationBase : IAnnotation
     public string? GuidSegment { get; set; }
     public string Definition => "/**/";
 
+    [JsonConstructor] public AnnotationBase() { }
     public AnnotationBase(string fromLine) { }
-
+    
     public static bool IsValid()
     {
         return false;
